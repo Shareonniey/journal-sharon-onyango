bbe9933890ff0545e1564917fd9414bc58bb7c2e
Shareonniey <sharonadhiambo015@gmail.com>
2025-11-24 09:37:42 +0300

    modified Heaser.jsx

--- my-journal/src/components/Header.jsx
+++ my-journal/src/components/Header.jsx
@@ -2,26 +2,16 @@ import React from "react";
 
 function Header({ onNewEntryClick, showImportantOnly, onToggleImportant }) {
   return (
-    <header className="header">
+    <div className="header">
       <h1>My Journal</h1>
-
-      <div className="header-controls">
-        <label className="important-toggle">
-          <input
-            type="checkbox"
-            checked={showImportantOnly}
-            onChange={onToggleImportant}
-          />
-          Show Important Only
-        </label>
-
-        <button className="btn primary" onClick={onNewEntryClick}>
-          + New Entry
-        </button>
-      </div>
-    </header>
+      <button className="button new-entry-button" onClick={onNewEntryClick}>
+        + New Entry
+      </button>
+      <button className="button" onClick={onToggleImportant}>
+        {showImportantOnly ? "Show All" : "Show Important Only"}
+      </button>
+    </div>
   );
 }
 
 export default Header;
-
